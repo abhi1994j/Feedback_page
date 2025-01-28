8a514b668ef5fa68164fc89fdfcd940ff5659239
abhi1994j <abhibaban4@gmail.com>
2025-01-28 09:26:00 +0530

    full project

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const containerE1 = document.getElementById('container')
 let selectedRating = "";
 let container = document.getElementById("container")
 ratingEls.forEach((ele)=>{
-    console.log(ele);
+    // console.log(ele);
    ele.addEventListener("click" , (e)=>{
-        // console.log(e);
+        // console.log(e.target.style);
         removeActive();
        selectedRating = 
-          e.target.innertext || e.target.parentNode.innertext;
+          e.target.innerText || e.target.parentNode.innerText;
           e.target.classList.add("active");
           e.target.parentNode.classList.add("active");
    }); 
@@ -22,7 +22,7 @@ btn.addEventListener('click' , (e)=>{
        `<strong>Thank you!</strong>
        <br />
        <br />
-       <strong>Feddback: ${selectedRating}</strong>
+       <strong>Feedback: ${selectedRating}</strong>
        <br />
        <br />
        <span>We'll use your feedback to improve our customer support.</span>
@@ -32,5 +32,6 @@ btn.addEventListener('click' , (e)=>{
 function removeActive(){
     ratingEls.forEach((ele)=>{
         console.log(ele);
+        ele?.classList?.remove("active")
     })
 }
